d0a2a3a53c308bf23568cc16d20f247cc4459e98
John <plumecalm@gmail.com>
2025-06-27 18:43:38 +0100

    refactor: simplify errorHandler middleware by removing unused parameters

--- src/api/middlewares/errorHandler.js
+++ src/api/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 import { logger } from '../../utils/logger.js';
 
-export const errorHandler = (err, req, res, next) => {
+export const errorHandler = (err, res) => {
   logger.error('Error:', err.message, { stack: err.stack });
 
   const statusCode = err.statusCode || 500;
